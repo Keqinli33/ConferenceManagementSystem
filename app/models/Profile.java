fc30be5ad0fda2adcf9c2b257291ab53acfa504c
sxh <603217995@qq.com>
2017-03-27 01:26:15 -0700

    fix some bugs

--- app/models/Profile.java
+++ app/models/Profile.java
@@ -3,7 +3,41 @@ package models;
 /**
  * Created by sxh on 17/3/26.
  */
-public class Profile {
+public class Profile extends Model{
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    public Long id;
+
+    @ManyToOne
+    public Title title;
+
+    public String research;
+
+    public String firstName;
+
+    public String lastName;
+
+    public String position;
+
+    public String affiliation;
+
+    public String email;
+
+    public String phone;
+
+    public String fax;
+
+    public String address;
+
+    public String city;
+
+    public String country;
+
+    public String region;
+
+    public Long zipcode;
+
+    public String comment;
 
 }
