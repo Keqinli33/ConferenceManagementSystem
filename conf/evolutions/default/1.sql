18270f191bd924a5b56bc9fa48c91cb0eb8302f1
Ling <helen14.su@hotmail.com>
2017-03-27 18:20:23 -0700

    add user

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,55 +1,62 @@
-# --- First database schema
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
 
 # --- !Ups
 
 create table company (
-  id                        bigint not null,
-  name                      varchar(255),
-  constraint pk_company primary key (id))
-;
+  id                            bigint auto_increment not null,
+  name                          varchar(255),
+  constraint pk_company primary key (id)
+);
 
 create table computer (
-  id                        bigint not null,
-  name                      varchar(255),
-  introduced                timestamp,
-  discontinued              timestamp,
-  company_id                bigint,
-  constraint pk_computer primary key (id))
-;
+  id                            bigint auto_increment not null,
+  name                          varchar(255),
+  introduced                    datetime(6),
+  discontinued                  datetime(6),
+  company_id                    bigint,
+  constraint pk_computer primary key (id)
+);
 
 create table profile (
-  id                        bigint not null,
-  title                      varchar(255),
-  research                   varchar(255),
-  firstName                  varchar(255),
-  lastName                   varchar(255),
-  position                   varchar(255),
-  affiliation                varchar(255),
-  email                      varchar(255),
-  phone                      varchar(255),
-  fax                        varchar(255),
-  address                    varchar(500),
-  city                       varchar(255),
-  country                    varchar(255),
-  region                     varchar(255),
+  id                            bigint auto_increment not null,
+  title                         varchar(255),
+  research                      varchar(255),
+  firstname                     varchar(255),
+  lastname                      varchar(255),
+  position                      varchar(255),
+  affiliation                   varchar(255),
+  email                         varchar(255),
+  phone                         varchar(255),
+  fax                           varchar(255),
+  address                       varchar(255),
+  city                          varchar(255),
+  country                       varchar(255),
+  region                        varchar(255),
   zipcode                       bigint,
-  comment                   varchar(500),
-  constraint pk_profile primary key (id))
-;
+  comment                       varchar(255),
+  constraint pk_profile primary key (id)
+);
 
-create sequence company_seq start with 1000;
+create table user (
+  id                            bigint auto_increment not null,
+  username                      varchar(255),
+  password                      varchar(255),
+  email                         varchar(255),
+  security_question_answer1     varchar(255),
+  security_question_answer2     varchar(255),
+  security_question_answer3     varchar(255),
+  constraint pk_user primary key (id)
+);
 
-create sequence computer_seq start with 1000;
-
-create sequence profile_seq start with 1000;
-
-alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;
-create index ix_computer_company_1 on computer (company_id);
+alter table computer add constraint fk_computer_company_id foreign key (company_id) references company (id) on delete restrict on update restrict;
+create index ix_computer_company_id on computer (company_id);
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+alter table computer drop foreign key fk_computer_company_id;
+drop index ix_computer_company_id on computer;
 
 drop table if exists company;
 
@@ -57,11 +64,5 @@ drop table if exists computer;
 
 drop table if exists profile;
 
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists company_seq;
-
-drop sequence if exists computer_seq;
-
-drop sequence if exists profile_seq;
+drop table if exists user;
 
