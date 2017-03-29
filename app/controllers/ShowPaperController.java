2c55a49c99c7ab06a58f6b04b2a0a504165de384
keqinli <likeqing0301@gmail.com>
2017-03-29 15:04:58 -0700

    create files

---
+++ app/controllers/ShowPaperController.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Transaction;
+import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+
+import models.*;
+
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+/**
+ * Created by keqinli on 3/29/17.
+ */
+public class ShowPaperController extends Controller{
+
+    private FormFactory formFactory;
+
+    @Inject
+    public ShowPaperController(FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
+
+    /**
+     * This result directly redirect to application home.
+     */
+    public Result GO_HOME = Results.redirect(
+            routes.HomeController.list(0, "name", "asc", "")
+    );
+
+    /**
+     * Handle default path requests, redirect to computers list
+     */
+    public Result index() {
+        return GO_HOME;
+    }
+
+    public Result list(int page, String sortBy, String order, String filter) {
+        return ok(
+                views.html.list.render(
+                        Computer.page(page, 10, sortBy, order, filter),
+                        sortBy, order, filter
+                )
+        );
+    }
+
+}
