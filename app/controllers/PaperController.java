fab2d7b5046bedff5b3c938612d63eae1ccf649a
ShuangZhangD <zhangshuanghust@gmail.com>
2017-03-30 12:20:03 -0700

    submit paper

--- app/controllers/PaperController.java
+++ app/controllers/PaperController.java
@@ -14,6 +14,7 @@ import javax.inject.Inject;
 import javax.persistence.PersistenceException;
 import java.util.Date;
 import java.io.File;
+import org.apache.commons.mail.*;
 /**
  * Created by shuang on 3/29/17.
  */
