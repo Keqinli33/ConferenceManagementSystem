49ce1c9073334502a5a68fc63d8bc95760568801
keqinli <likeqing0301@gmail.com>
2017-03-30 21:43:12 -0700

    Merge branches 'sprint1' and 'submitpaper' of https://github.com/cmusv-sc/18655-Spring-2017-Team-3 into submitpaper
    # Conflicts:
    #	app/views/showmypaper.scala.html

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -68,7 +68,7 @@ create table paper (
 );
 
 create table profile (
-  userid                        bigint not null,
+  userid                        bigint auto_increment not null,
   title                         varchar(255),
   research                      varchar(255),
   firstname                     varchar(255),
