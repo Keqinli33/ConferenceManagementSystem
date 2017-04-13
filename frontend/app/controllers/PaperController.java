fca44c591346a297c825f641743c383a2b1cd3a7
ShuangZhangD <zhangshuanghust@gmail.com>
2017-04-13 14:40:49 -0700

    Merge branch 'submitpaper' of https://github.com/cmusv-sc/18655-Spring-2017-Team-3 into profile
    # Conflicts:
    #	app/views/showmypaper.scala.html

--- frontend/app/controllers/PaperController.java
+++ frontend/app/controllers/PaperController.java
@@ -73,6 +73,7 @@ public class PaperController extends Controller {
         return res.thenApply(response -> {
 
             JsonNode ret = response.asJson();
+            System.out.println("response from edit" + ret);
             Paper savedPaper = new Paper();
             savedPaper.title = ret.get("title").asText();
             savedPaper.contactemail = ret.get("contactemail").asText();
@@ -135,47 +136,7 @@ public class PaperController extends Controller {
 //                if(!newPaperData.contactemail.equals(newPaperData.confirmemail)){
 //                    return badRequest(views.html.editPaper.render(id, paperForm));
 //                }
-//                savedPaper.title = newPaperData.title;
-//                savedPaper.contactemail = newPaperData.contactemail;
-//                savedPaper.firstname1 = newPaperData.firstname1;
-//                savedPaper.lastname1 = newPaperData.lastname1;
-//                savedPaper.email1 = newPaperData.email1;
-//                savedPaper.affilation1 = newPaperData.affilation1;
-//                savedPaper.firstname2 = newPaperData.firstname2;
-//                savedPaper.lastname2 = newPaperData.lastname2;
-//                savedPaper.email2 = newPaperData.email2;
-//                savedPaper.affilation2 = newPaperData.affilation2;
-//                savedPaper.firstname3 = newPaperData.firstname3;
-//                savedPaper.lastname3 = newPaperData.lastname3;
-//                savedPaper.email3 = newPaperData.email3;
-//                savedPaper.affilation3 = newPaperData.affilation3;
-//                savedPaper.firstname4 = newPaperData.firstname4;
-//                savedPaper.lastname4 = newPaperData.lastname4;
-//                savedPaper.email4 = newPaperData.email4;
-//                savedPaper.affilation4 = newPaperData.affilation4;
-//                savedPaper.firstname5 = newPaperData.firstname5;
-//                savedPaper.lastname5 = newPaperData.lastname5;
-//                savedPaper.email5 = newPaperData.email5;
-//                savedPaper.affilation5 = newPaperData.affilation5;
-//                savedPaper.firstname6 = newPaperData.firstname6;
-//                savedPaper.lastname6 = newPaperData.lastname6;
-//                savedPaper.email6 = newPaperData.email6;
-//                savedPaper.affilation6 = newPaperData.affilation6;
-//                savedPaper.firstname7 = newPaperData.firstname7;
-//                savedPaper.lastname7 = newPaperData.lastname7;
-//                savedPaper.email7 = newPaperData.email7;
-//                savedPaper.affilation7 = newPaperData.affilation7;
-//                savedPaper.otherauthor = newPaperData.otherauthor;
-//                savedPaper.candidate = newPaperData.candidate;
-//                savedPaper.volunteer = newPaperData.volunteer;
-//                savedPaper.paperabstract = newPaperData.paperabstract;
-//                savedPaper.ifsubmit = newPaperData.ifsubmit;
-//                savedPaper.format = newPaperData.format;
-//                savedPaper.papersize = newPaperData.papersize;
-//                savedPaper.conference = newPaperData.conference;
-//                savedPaper.topic = newPaperData.topic;
-//                savedPaper.status = newPaperData.status;
-//                savedPaper.date = newPaperData.date;
+
         JsonNode json = Json.newObject()
                 .put("title", newPaper.title)
                 .put("contactemail",newPaper.contactemail)
@@ -214,10 +175,10 @@ public class PaperController extends Controller {
                 .put("topic", newPaper.topic);
                 
 
-        CompletionStage<WSResponse> res = ws.url("http://localhost:9000/papers"+id).post(json);
+        CompletionStage<WSResponse> res = ws.url("http://localhost:9000/papers/"+id).post(json);
         return res.thenApplyAsync(response -> {
             String ret = response.getBody();
-            System.out.println("here is "+ret);
+            System.out.println("response from update "+ret);
 //            if(paperForm.hasErrors()) {
 //                return badRequest(views.html.createPaper.render(paperForm));
 //            }
