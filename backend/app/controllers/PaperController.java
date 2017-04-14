58980988bf7e767e713ad917c74d4f8c8275cb2b
ShuangZhangD <zhangshuanghust@gmail.com>
2017-04-14 14:21:50 -0700

    Merge branch 'new_master' of https://github.com/cmusv-sc/18655-Spring-2017-Team-3 into conference
    # Conflicts:
    #	backend/conf/routes

--- backend/app/controllers/PaperController.java
+++ backend/app/controllers/PaperController.java
@@ -187,10 +187,31 @@ public class PaperController extends Controller {
         return ok("save successfully");
     }
     public Result uploadFile(Long id) {
-        Form<Paper> paperForm = formFactory.form(Paper.class);
-        return ok(
-                views.html.selectFile.render(id, paperForm)
-        );
+        Form<Paper> paperForm = formFactory.form(Paper.class).bindFromRequest();
+
+        Transaction txn = Ebean.beginTransaction();
+        try {
+            Paper savedPaper = Paper.find.byId(id);
+            if (savedPaper != null) {
+                Paper newPaperData = paperForm.get();
+//                if(!newPaperData.contactemail.equals(newPaperData.confirmemail)){
+//                    return badRequest(views.html.editPaper.render(id, paperForm));
+//                }
+
+                savedPaper.ifsubmit = newPaperData.ifsubmit;
+                savedPaper.format = newPaperData.format;
+                savedPaper.papersize = newPaperData.papersize;
+//                savedPaper.status = newPaperData.status;
+//                savedPaper.date = newPaperData.date;
+
+                savedPaper.update();
+                txn.commit();
+            }
+        } finally {
+            txn.end();
+        }
+
+        return ok("update successfully");
     }
     public Result selectFile(Long id) {
         Form<Paper> paperForm = formFactory.form(Paper.class).bindFromRequest();
