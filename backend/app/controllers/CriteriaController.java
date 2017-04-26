06e7ff631277778c715624d35e19527700ab435d
keqinli <likeqing0301@gmail.com>
2017-04-25 21:42:37 -0700

    criteria # Conflicts: #	frontend/app/controllers/ShowPaperController.java

---
+++ backend/app/controllers/CriteriaController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import play.data.format.Formats;
+import play.mvc.Controller;
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Transaction;
+import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+import play.mvc.Result;
+import play.mvc.Http;
+
+import models.*;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import com.sun.jersey.multipart.FormDataMultiPart;
+import com.sun.jersey.multipart.file.FileDataBodyPart;
+import javax.ws.rs.core.MediaType;
+
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+import java.util.Date;
+import java.io.File;
+import org.apache.commons.mail.*;
+import org.apache.commons.io.FileUtils;
+import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.Json;
+import play.libs.ws.*;
+import java.util.concurrent.CompletionStage;
+
+public class CriteriaController extends Controller {
+
+}
---
+++ backend/app/models/Criteria.java
@@ -0,0 +1,33 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import com.avaje.ebean.Model;
+import play.data.format.*;
+import play.data.validation.*;
+
+import com.avaje.ebean.*;
+
+import com.avaje.ebean.Model;
+
+@Entity
+public class Criteria extends Model {
+    private static final long serialVersionUID = 1L;
+    @Id
+    public long id;
+    public String label;
+    public String explanations;
+    public String weight;
+
+    public static Find<Long,Criteria> find = new Find<Long,Criteria>(){};
+
+    public static List<Criteria> GetMyCriteria(Long criteriaId){
+        List<Criteria> results =
+                find.where()
+                        .eq("id",criteriaId)
+                        .findList();
+        return results;
+    }
+
+}
